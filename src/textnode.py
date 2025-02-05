335c887eca1014cebbaee7ff9760351976374f80
Nikolai Lønberg <niklonberg@gmail.com>
2025-02-05 16:10:51 +0100

    add TextNode class

--- src/textnode.py
+++ src/textnode.py
@@ -7,3 +7,23 @@ class TextType(Enum):
   CODE = 'code'
   LINK = 'link'
   IMAGE = 'image'
+
+class TextNode():
+  def __init__(self, text, text_type: TextType, url=None):
+    if not isinstance(text_type, TextType):
+      raise TypeError("text_type must be a TextType enum member")
+    self.text = text
+    self.text_type = text_type
+    self.url = url
+
+
+  def __eq__(self, other):
+    return (self.text == other.text and
+            self.text_type == other.text_type and
+            self.url == other.url)
+  
+  
+  def __repr__(self):
+    url_str = f"'{self.url}'" if self.url is not None else "None"
+    return f"TextNode('{self.text}', '{self.text_type.value}', '{url_str}')"
+    
