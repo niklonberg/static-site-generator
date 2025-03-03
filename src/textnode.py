91cbc58c344302c2f6ead4c03dc6c2dd2c8a8cad
Nikolai Lønberg <niklonberg@gmail.com>
2025-03-03 17:41:24 +0100

    add text_node_to_html_node fn

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
   TEXT = 'text'
@@ -16,14 +17,29 @@ class TextNode():
     self.text_type = text_type
     self.url = url
 
-
   def __eq__(self, other):
     return (self.text == other.text and
             self.text_type == other.text_type and
             self.url == other.url)
-  
-  
+    
   def __repr__(self):
     url_str = f"'{self.url}'" if self.url is not None else "None"
     return f"TextNode('{self.text}', '{self.text_type.value}', '{url_str}')"
-    
+
+
+def text_node_to_html_node(text_node: TextNode):
+  match text_node.text_type:
+    case TextType.TEXT:
+      return LeafNode(None, text_node.text)
+    case TextType.BOLD:
+      return LeafNode("b", text_node.text)
+    case TextType.ITALIC:
+      return LeafNode("i", text_node.text)
+    case TextType.CODE:
+      return LeafNode("code", text_node.text)
+    case TextType.LINK:
+      return LeafNode("a", text_node.text, {"href": text_node.url})
+    case TextType.IMAGE:
+      return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    case _:
+      raise Exception("Invalid TextNode type")
