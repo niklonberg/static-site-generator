461361154bdf87d71a7d3bdcd16777d7ac69746d
Nikolai Lønberg <niklonberg@gmail.com>
2025-03-03 12:42:06 +0100

    Fix ParentNode constructor to correctly pass parameters to parent class

--- src/parentnode.py
+++ src/parentnode.py
@@ -2,7 +2,7 @@ from htmlnode import HTMLNode
 
 class ParentNode(HTMLNode):
   def __init__(self, tag, children, props=None):
-    super().__init__(tag, children, props)
+    super().__init__(tag=tag, children=children, props=props)
 
   def to_html(self):
     if self.tag is None:
