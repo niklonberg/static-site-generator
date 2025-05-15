a7b1ac7fa4920389ea15862a753955ffab201e0b
Nikolai Lønberg <niklonberg@gmail.com>
2025-05-15 11:57:25 +0200

    add split delimiter functionality

---
+++ src/test_inline_markdown.py
@@ -0,0 +1,45 @@
+import unittest
+from inline_markdown import split_nodes_delimiter
+
+from textnode import TextNode, TextType
+   
+class TestInlineMarkdown(unittest.TestCase):
+   def test_delim_bold(self):
+      node = TextNode('This is text with a **bolded** word', TextType.TEXT)
+      new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+      self.assertEqual(
+       [
+          TextNode("This is text with a ", TextType.TEXT),
+          TextNode("bolded", TextType.BOLD),
+          TextNode(" word", TextType.TEXT),
+       ],
+       new_nodes
+    )
+  
+   def test_double_delim_bold(self):
+      node = TextNode('This is text with a **bolded** word and **another**', TextType.TEXT)
+      new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+      self.assertEqual(
+       [
+          TextNode("This is text with a ", TextType.TEXT),
+          TextNode("bolded", TextType.BOLD),
+          TextNode(" word and ", TextType.TEXT),
+          TextNode("another", TextType.BOLD)
+       ],
+       new_nodes
+    )
+
+   def test_delim_italic(self):
+      node = TextNode("This is text with an _italic_ word", TextType.TEXT)
+      new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+      self.assertListEqual(
+        [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word", TextType.TEXT),
+        ],
+        new_nodes,
+    )
+
+if __name__ == "__main__":
+    unittest.main()
