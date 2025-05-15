4a11d7c35c97c910cf3f0113ea6f799901050183
Nikolai Lønberg <niklonberg@gmail.com>
2025-05-15 13:39:22 +0200

    add code test

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -2,7 +2,7 @@ import unittest
 from inline_markdown import split_nodes_delimiter
 
 from textnode import TextNode, TextType
-   
+
 class TestInlineMarkdown(unittest.TestCase):
    def test_delim_bold(self):
       node = TextNode('This is text with a **bolded** word', TextType.TEXT)
@@ -40,6 +40,18 @@ class TestInlineMarkdown(unittest.TestCase):
         ],
         new_nodes,
     )
+      
+   def test_delim_code(self):
+      node = TextNode("This is text with a `code block` word", TextType.TEXT)
+      new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+      self.assertListEqual(
+        [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ],
+        new_nodes,
+      )
 
 if __name__ == "__main__":
     unittest.main()
